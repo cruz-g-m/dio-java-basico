5f2f450fc4e60d0057803eb5447448a05fd86475
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-10-02 10:20:26 -0300

    aula sobre tipos primitivos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;             //tipos de variaveis
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //casting
+        
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+
+        final double VALOR_DE_PI = 3.14;  // Variavel constante
+
+    
     }
+
 }
