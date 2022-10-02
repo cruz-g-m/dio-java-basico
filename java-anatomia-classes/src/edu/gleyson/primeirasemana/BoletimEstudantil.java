f00724db2f5909965fec15d843dc1cda5ada3f29
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-10-02 09:39:48 -0300

    atualização de diretórios

---
+++ java-anatomia-classes/src/edu/gleyson/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package edu.gleyson.primeirasemana;
+public class BoletimEstudantil {
+   public static void main(String[] args) {
+        int mediaFinal = 5;
+        if (mediaFinal < 6)
+             System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+             System.out.println("PROVA DE MINERVA");
+        else
+             System.out.println("APROVADO");
+    }
+   } 
+
